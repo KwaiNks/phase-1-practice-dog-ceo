849a454164d933f0fdce213bdfb91455eac7eed6
Richmond Nkansah <richmondnkansah@gmail.com>
2022-10-29 00:47:45 -0400

    commit tree 7f72d5fcb713dfd0bff5d15448ec8d50d309c4f9

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgContainer = document.querySelector("#dog_image_container")
+const image = document.createElement("img")
+
+fetch(imgUrl)
+ .then(res => res.json())
+ .then(data => {
+    data.forEach(element => {
+        image.append(element)
+      imgContainer.append(image)
+    });
+ })
